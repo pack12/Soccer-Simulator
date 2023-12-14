d21b149eb1ccb26bc524a13ee52f3f42b1c70af8
J.Kettula <jkettula@ycp.edu>
2023-12-13 21:00:35 -0500

    Generate random schedule for all 20 teams (not fully functional)

--- database.py
+++ database.py
@@ -1,22 +1,32 @@
 import sqlite3
 import json
 import random
+import match
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 import numpy as np
 
+"""Create connection object to soccer.db"""
 def connect():
 
 
     con = sqlite3.connect("soccer.db")
     return con
+
+"""Create datatables"""
 def create_tables(con):
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS player(playerid,first,last,overall,team)")
     cur.execute("CREATE TABLE IF NOT EXISTS teams(teamid, name,facilities,managerid,scouting,finances)")
     cur.execute("CREATE TABLE IF NOT EXISTS managers(managerid, first,last,rating, team)")
+    cur.execute("CREATE TABLE IF NOT EXISTS schedule(teamid, week_1, week_2, week_3, week_4, week_5, week_6, week_7, week_8, week_9,"
+                "week_10, week_11, week_12, week_13, week_14, week_15, week_16, week_17, week_18, week_19, week_20, week_21,"
+                "week_22, week_23, week_24, week_25, week_26, week_27, week_28, week_29, week_30, week_31, week_32, week_33, "
+                "week_34, week_35, week_36, week_37, week_38)")
+    #cur.execute("CREATE TABLE IF NOT EXISTS player_attributes(playerid,)")
 
+"""Use Json to create Teams datatable"""
 def populate_teams(con):
     cur = con.cursor()
     res = cur.execute("SELECT * FROM teams")
@@ -49,6 +59,7 @@ def populate_teams(con):
                 data = (manager_id, name, team_id)
 
                 cur.execute(sql_update_query, data)
+
 def populate_players(con):
     cur = con.cursor()
     res = cur.execute("SELECT * FROM player")
@@ -114,7 +125,7 @@ def populate_players(con):
 
                 cur.executemany("INSERT INTO player VALUES(?,?,?,?,?)", data)
                 id += 1
-            print(f'Team: {team} Sum rating: {sum(sum_overall)}')
+            # print(f'Team: {team} Sum rating: {sum(sum_overall)}')
 
 def populate_managers(con):
     cur = con.cursor()
@@ -148,7 +159,9 @@ def drop_tables(con):
     cur.execute("DROP TABLE player")
     cur.execute("DROP TABLE teams")
     cur.execute("DROP TABLE managers")
+    cur.execute("DROP TABLE schedule")
 
+"""Functionality of executing sqlite command line"""
 def execute_user_command(con, usr_input):
     cur = con.cursor()
     try:
@@ -162,3 +175,110 @@ def execute_user_command(con, usr_input):
         for i in res.fetchall():
 
             print(i)
+
+"""Helper function to get team info"""
+def get_team_info(con,team):
+    cur = con.cursor()
+    team_info = cur.execute("select * from teams where teams.name = ?",(team,))
+    return team_info
+    # for i in team_info.fetchall():
+    #     print(i[0])
+
+
+def generate_schedule(con):
+
+    cur = con.cursor()
+    teams = cur.execute("select teams.name from teams")
+    # teams = teams.fetchall()
+    rnd = random.randint(0, 19)
+
+    #Prints Teams: <sqlite3.Cursor object
+    print(f'Teams: {teams}')
+
+
+    lst_team = []
+    for i in teams.fetchall():
+        # print(i[0])
+        """Add names of team to lst_team"""
+        lst_team.append(i[0])
+
+    original_teams = lst_team
+    print(f'Original 1: {original_teams}')
+
+
+
+
+    """My assumption is that this part of the code is used to use original list and create a schedule"""
+    for i in range(len(original_teams)):
+        home_team = original_teams[i]
+        low = 0
+        week = 1
+        team_id = get_team_info(con, home_team)
+        team_id = team_id.fetchone()[0]
+        # print(f'Teaminfo: {team_id}')
+
+        """Double the list, so it goes Arsenal...Wolves, Arsenal ... Wolves"""
+        lst_team = original_teams * 2
+
+        for j in range(len(lst_team)):
+
+            rnd = random.randint(low,len(lst_team)-1)
+            # print(f'Rand: {rnd}')
+            temp = lst_team[rnd]
+
+            #Swap teams with random index
+            lst_team[rnd] = lst_team[j]
+            lst_team[j] = temp
+
+
+            """My assumption is that this part is to prevent teams playing back-to-back
+            so Arsenal can't play Crystal Palace Week 18, and then play Crystal Palace Week 19"""
+            if j+1 < 39 and lst_team[j] == lst_team[j+1]:
+                # print("When does this execute?")
+
+
+                # Do another swap
+                rnd = random.randint(low, 39)
+
+                # swap teams with random index
+                temp = lst_team[rnd]
+                lst_team[rnd] = lst_team[i]
+                lst_team[i] = temp
+
+            if week == 39:
+                week -= 1
+            insert_into_schedule(con,team_id,week,lst_team[j])
+            scheddd = cur.execute("select * from schedule")
+            for row in scheddd.fetchall():
+                print(f'ROW: {row}')
+            week+=1
+
+            low+=1
+
+        for k in range(2):
+            if home_team in lst_team:
+                lst_team.remove(home_team)
+        print(f'{home_team} -Iteration {i} {lst_team}')
+
+
+
+
+
+"""Inserts teamid values into schedule datatable"""
+def generate_initial_schedule_value(cursor):
+    cur = cursor
+    #Generate all of the team_ids
+    for i in range(1,21):
+        # print(i)
+        cur.execute("insert into schedule (teamid) values(?)",(i,))
+
+def insert_into_schedule(con, home_id, rand_num, other_team):
+    cur = con.cursor()
+    week_str = "week_" + str(rand_num)
+    print(week_str)
+    print(other_team)
+    command_line = f'update schedule set {week_str} = ? where teamid = ?'
+    cur.execute(command_line,(other_team, home_id))
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import database
+
+
 def game():
     db = database
     con = db.connect()
@@ -6,6 +8,9 @@ def game():
     db.populate_managers(con)
     db.populate_teams(con)
     db.populate_players(con)
+    db.generate_initial_schedule_value(con)
+    db.generate_schedule(con)
+
     user_input = str(input("Enter SQL Command or Exit: "))
     while user_input != "exit":
         db.execute_user_command(con, user_input)
@@ -13,7 +18,7 @@ def game():
     #testing purposes
     db.drop_tables(con)
 
-    pass
+
 
 if __name__ == "__main__":
     game()
